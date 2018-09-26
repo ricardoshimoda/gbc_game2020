87bff820f91256ebd3ae96da0cb3eee5ce5d31cb
Ricardo Shimoda <rshimoda@gmail.com>
2018-09-26 08:14:19 -0400

    Frist Version of Burger Time
    Now we have game UI!!

---
+++ GAME2020_M1_RSN_BT/Source/GAME2020_M1_RSN_BT.Target.cs
@@ -0,0 +1,13 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class GAME2020_M1_RSN_BTTarget : TargetRules
+{
+	public GAME2020_M1_RSN_BTTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Game;
+		ExtraModuleNames.Add("GAME2020_M1_RSN_BT");
+	}
+}
---
+++ GAME2020_M1_RSN_BT/Source/GAME2020_M1_RSN_BT/GAME2020_M1_RSN_BTGameMode.cpp
@@ -0,0 +1,15 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#include "GAME2020_M1_RSN_BTGameMode.h"
+#include "GAME2020_M1_RSN_BTCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+AGAME2020_M1_RSN_BTGameMode::AGAME2020_M1_RSN_BTGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/SideScrollerCPP/Blueprints/SideScrollerCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
